3095b629dd33cbd356647bfa983d91cd1fee7dc5
olamide Toluwase <olamide.toluwase@skillslab.sidmach.com>
2022-01-05 13:53:15 +0100

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div style={{backgroundColor:'blue'}}>
+            <p style={{color:'white'}}> Designed and Developed By</p>
+            <p style={{color:'white'}}>O.Toluwase</p>
+        </div>
+    )
+}
+
+export default Footer
